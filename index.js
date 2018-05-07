a5de27c853a9c6a63337b9ad3f85cd8f5a14562e
Anallely Curiel <anallely.curiel@gmail.com>
2018-05-07 16:31:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // write your code below!
 
 var name = 'Anallely'
-var height = "30"
+var height = 30
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+1)
+//console.log(parseInt(height)+1)
+console.log(height+1)
